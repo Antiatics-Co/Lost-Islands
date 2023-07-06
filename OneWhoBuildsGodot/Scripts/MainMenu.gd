835ef201f114a15948547d7cad7ce05bf7ac1d80
OneWhoBuilds <aidcox2008@gmail.com>
2023-07-06 17:58:03 -0400

    mainmenu
    currently only the start button works, need to quit godot from task manager, will be working on a pause menu, and quit button next, along with making a better design for the main menu

---
+++ OneWhoBuildsGodot/Scripts/MainMenu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+@export var mainGameScene : PackedScene
+
+
+func _on_start_button_up():
+	get_tree().change_scene_to_packed(mainGameScene)
