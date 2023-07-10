dc65bcd7953f66a2a4949a1843b4b7986aab2b0f
OneWhoBuilds <aidcox2008@gmail.com>
2023-07-10 17:29:37 -0400

    Fixed quit button functionality
    Quit button now quits

--- OneWhoBuildsGodot/Scripts/MainMenu.gd
+++ OneWhoBuildsGodot/Scripts/MainMenu.gd
@@ -14,4 +14,8 @@ func _process(delta):
 
 
 func _on_start_button_up():
-	get_tree().change_scene_to_packed(mainGameScene)
+	get_tree().change_scene_to_packed(mainGameScene)	
+
+
+func _on_quit_button_down():
+	get_tree().quit()
